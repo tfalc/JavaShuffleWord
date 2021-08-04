4cd639c1672ac558c17bba5507a3360365155a07
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-08-04 17:48:05 -0300

    Initial commit

---
+++ src/main/java/com/tfalc/shuffleword/shuffleword/ShufflewordApplication.java
@@ -0,0 +1,13 @@
+package com.tfalc.shuffleword.shuffleword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShufflewordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShufflewordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfalc/shuffleword/shuffleword/ShufflewordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfalc.shuffleword.shuffleword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShufflewordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
